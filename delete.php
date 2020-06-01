77e992d7a13a0f87c0d09e619d81ac9cc236311e
drewb73 <bartondrew5@gmail.com>
2020-06-01 09:16:32 -0700

    CRUD files

---
+++ delete.php
@@ -0,0 +1,14 @@
+<?php
+
+	include('conn.php');
+
+	$id = $_GET['q'];
+
+	$query = "DELETE FROM contacts WHERE id = '$id'";
+
+	if (mysqli_query($conn, $query)) {
+		header("location: index.php");
+
+	} else {
+		echo 'Cannot Delete';
+	}
